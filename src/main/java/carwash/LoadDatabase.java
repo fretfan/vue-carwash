bb8cfad60c72e36cf3f52900815265aaa8865457
Sergei Kossik <sergei.kossik@ignite.ee>
2019-03-17 14:49:05 +0200

    Revert to getters, for Kotlin compatibility

--- src/main/java/carwash/LoadDatabase.java
+++ src/main/java/carwash/LoadDatabase.java
@@ -20,33 +20,29 @@ public class LoadDatabase {
   @PostConstruct
   public void initDatabase() {
     log.info("Start database inserts");
-    var depot1 = Depot.builder()
-      .name("Sam's carwash")
-      .address("Mon Str 5")
-      .numberOfWorkers(5)
-      .build();
 
+    var depot1 = new Depot();
+    depot1.setName("Sam's carwash");
+    depot1.setAddress("Mon Str 5");
+    depot1.setNumberOfWorkers(5);
     depotRepository.save(depot1);
 
-    var depot2 = Depot.builder()
-      .name("Anna's carwash")
-      .address("Tue Str 12")
-      .numberOfWorkers(2)
-      .build();
+    var depot2 = new Depot();
+    depot2.setName("Anna's carwash");
+    depot2.setAddress("Tue Str 12");
+    depot2.setNumberOfWorkers(2);
     depotRepository.save(depot2);
 
-    var depot3 = Depot.builder()
-      .name("Franks's carwash")
-      .address("Middle of nowhere 321")
-      .numberOfWorkers(10)
-      .build();
+    var depot3 = new Depot();
+    depot3.setName("Franks's carwash");
+    depot3.setAddress("Middle of nowhere 321");
+    depot3.setNumberOfWorkers(10);
     depotRepository.save(depot3);
 
-    var depot4 = Depot.builder()
-      .name("Smith's carwash")
-      .address("Very close str 67")
-      .numberOfWorkers(8)
-      .build();
+    var depot4 = new Depot();
+    depot4.setName("Smith's carwash");
+    depot4.setAddress("Very close str 67");
+    depot4.setNumberOfWorkers(8);
     depotRepository.save(depot4);
 
     log.info("Finish database inserts");
